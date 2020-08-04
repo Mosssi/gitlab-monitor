062644045d6ae3652b5f348e5f921f13c4f3f0e4
Mohsen Esmaeili <msn.esmaeili@gmail.com>
2020-08-04 19:01:57 +0430

    More on installation and auto-start

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include "components/ApplicationWidget.h"
 #include "utilities/GuiManager.h"
 
+
+
 int main(int argc, char ** argv) {
     QApplication application(argc, argv);
     QApplication::setFont(GuiManager::applicationFont());
-    QApplication::setOrganizationName("mesmaeili");
+    QApplication::setOrganizationName("Mosssi");
     QApplication::setApplicationName("gitlab-monitor");
     QFontDatabase::addApplicationFont(":/fonts/gitlab-monitoring.ttf");
 
